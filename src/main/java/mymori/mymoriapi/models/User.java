c06287e91f286ad2e8cd542b70c372df8f524e0f
victor <victorangelograca@hotmail.com>
2021-03-08 11:04:43 -0300

    minor fixes

--- src/main/java/mymori/mymoriapi/models/User.java
+++ src/main/java/mymori/mymoriapi/models/User.java
@@ -2,11 +2,10 @@ package mymori.mymoriapi.models;
 
 
 import javax.persistence.*;
-import java.io.Serializable;
 
 @Entity
 @Table(name = "users")
-public class User implements Serializable {
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
